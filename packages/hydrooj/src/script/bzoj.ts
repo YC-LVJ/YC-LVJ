64ae2853d5eec7925cbb8bf2902fa0b3a2cb0a80
StupidQu <xiaoququ@zshfoj.com>
2023-12-08 23:13:58 +0800

    core: fix bzoj

--- packages/hydrooj/src/script/bzoj.ts
+++ packages/hydrooj/src/script/bzoj.ts
@@ -7,6 +7,7 @@ import { AdmZip, ProblemModel } from '../plugin-api';
 async function run(_, report: Function) {
     const problems = fs.readdirSync('/home/ycrrjy/lydsy/');
     for (const problem of problems) {
+        if (!problem.includes('.zip')) continue;
         const pid = problem.replace('.zip', '');
         const npid = await ProblemModel.add('bzoj', `BZOJ${pid}`, `BZOJ${pid}`, `BZOJ${pid}`, 2);
         const zip = new AdmZip(`/home/ycrrjy/lydsy/${problem}`);
