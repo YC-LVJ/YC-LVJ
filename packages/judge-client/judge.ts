1dc6bec7e318bd3120e953cb68743f49dad4b570
Tairui Qu <XiaoQuQu_CN@outlook.com>
2025-09-13 21:22:12 +0800

    fix: vjudge client

--- packages/judge-client/judge.ts
+++ packages/judge-client/judge.ts
@@ -1,55 +1,103 @@
 /* eslint-disable no-await-in-loop */
-import axios from 'axios';
+import axios from "axios";
 import {
-    JudgeHandler, RecordModel, sleep, STATUS, SystemModel, TaskModel,
-} from 'hydrooj';
-import { Logger } from 'hydrooj/src/logger';
+    JudgeHandler,
+    RecordModel,
+    sleep,
+    STATUS,
+    SystemModel,
+    TaskModel,
+} from "hydrooj";
+import { Logger } from "hydrooj/src/logger";
 
-const logger = new Logger('judge-client');
-const judgingStatus = [STATUS.STATUS_FETCHED, STATUS.STATUS_COMPILING, STATUS.STATUS_JUDGING, STATUS.STATUS_WAITING];
+const logger = new Logger("judge-client");
+const judgingStatus = [
+    STATUS.STATUS_FETCHED,
+    STATUS.STATUS_COMPILING,
+    STATUS.STATUS_JUDGING,
+    STATUS.STATUS_WAITING,
+];
 
 export async function apply() {
-    if (process.env.NODE_APP_INSTANCE !== '0') return;
+    if (process.env.NODE_APP_INSTANCE !== "0") return;
     if (process.env.HYDRO_CLI) return;
 
-    const host = 'https://zshfoj.com';
-    const token = SystemModel.get('judgeserver.token');
+    const host = "https://lvjcn.tboj.cn/";
+    const token = SystemModel.get("judgeserver.token");
 
     async function judge(task) {
         logger.info(`Start judging ${task.rid}`);
         const rdoc = await RecordModel.get(task.domainId, task.rid);
         task = Object.assign(rdoc, task);
-        const next = (payload) => JudgeHandler.next({
-            ...payload, rid: task.rid, rdoc, domainId: task.domainId,
-        });
-        const end = (payload) => JudgeHandler.end({
-            ...payload, rid: task.rid, rdoc, domainId: task.domainId,
-        });
+        const next = (payload) =>
+            JudgeHandler.next({
+                ...payload,
+                rid: task.rid,
+                rdoc,
+                domainId: task.domainId,
+            });
+        const end = (payload) =>
+            JudgeHandler.end({
+                ...payload,
+                rid: task.rid,
+                rdoc,
+                domainId: task.domainId,
+            });
         const client = axios.create({
             headers: {
-                Accept: 'application/json',
+                Accept: "application/json",
             },
             baseURL: host,
         });
         await next({ status: STATUS.STATUS_FETCHED });
         async function fetchResult() {
-            const { data } = await client.post('/judge-server/judge', {
-                token,
-                pid: task.target,
-                code: encodeURI(task.code),
-                language: 'cc.cc14o2',
-            });
+            let submitRetries = 3;
+            let data: any = null;
+            while (submitRetries > 0) {
+                try {
+                    const _ = await client.post("/judge-server/judge", {
+                        token,
+                        pid: task.target,
+                        code: encodeURI(task.code),
+                        language: "cc.cc14o2",
+                    });
+                    data = _.data;
+                    break;
+                } catch {
+                    submitRetries--;
+                }
+            }
+
             if (!data.success) {
                 end({ status: STATUS.STATUS_SYSTEM_ERROR, message: data.message });
                 return;
             }
             const rid = data.rid;
-            next({ status: STATUS.STATUS_JUDGING, message: '由 LVJ 提供评测服务。' });
+            next({ status: STATUS.STATUS_JUDGING });
             let done = false;
             let tries = 0;
-            while (!done && tries <= 1000) {
-                await sleep(300);
-                const srdoc = (await client.get(`/judge-server/record?token=${token}&rid=${rid}`)).data;
+            while (!done && tries <= 300) {
+                await sleep(1000);
+                let srdoc: any;
+                let queryFailed = 0;
+                while (queryFailed <= 3) {
+                    try {
+                        srdoc = (
+                            await client.get(`/judge-server/record?token=${token}&rid=${rid}`)
+                        ).data;
+                        break;
+                    } catch {
+                        queryFailed++;
+                        if (queryFailed > 3) {
+                            end({
+                                status: STATUS.STATUS_SYSTEM_ERROR,
+                                message: "Failed to get judge result from server.",
+                            });
+                            return;
+                        }
+                        await sleep(500); // 等待一下再重试
+                    }
+                }
                 if (judgingStatus.includes(srdoc.status)) {
                     tries++;
                     continue;
@@ -64,17 +112,23 @@ export async function apply() {
                 });
                 done = true;
             }
-            if (!done) end({ status: STATUS.STATUS_SYSTEM_ERROR, message: 'Judging timeout exceeded 300s.' });
-            logger.info('Judge finished');
+            if (!done)
+                end({
+                    status: STATUS.STATUS_SYSTEM_ERROR,
+                    message: "Judging timeout exceeded 300s.",
+                });
+            logger.info("Judge finished");
         }
-        try {
-            fetchResult().then(() => {
-                logger.info(`End judging ${task.rid}`);
-            });
-        } catch (e) {
-            end({ status: STATUS.STATUS_SYSTEM_ERROR, message: e.message });
-        }
+        fetchResult().catch((error) => {
+            end({ status: STATUS.STATUS_SYSTEM_ERROR, message: error.message });
+        }).then(() => {
+            logger.info(`End judging ${task.rid}`);
+        });
     }
-    TaskModel.consume({ type: 'remotejudge', subType: 'judgeclient' }, judge, false);
-    logger.info('Judge client is now running!');
+    TaskModel.consume(
+        { type: "remotejudge", subType: "judgeclient" },
+        judge,
+        false
+    );
+    logger.info("Judge client is now running!");
 }
