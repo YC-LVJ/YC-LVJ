97bcc49623ea12f94b2dc1abe1b5b6baa22d3bc4
Tairui Qu <XiaoQuQu_CN@outlook.com>
2025-09-13 21:54:45 +0800

    feat: luogu-style difficulty rendering

--- packages/ui-default/backendlib/template.js
+++ packages/ui-default/backendlib/template.js
@@ -112,6 +112,61 @@ class Nunjucks extends nunjucks.Environment {
       console.log(self);
       return self;
     });
+
+    // Luogu-style difficulty mappings
+    const PROBLEMS_DIFFICULTY = [
+      '暂无评定',
+      '入门',
+      '普及-',
+      '普及/提高-',
+      '普及+/提高',
+      '提高+/省选-',
+      '省选/NOI-',
+      'NOI/NOI+',
+      'NOI+/CSTC',
+      'NOI+/CSTC',
+      'NOI+/CSTC',
+    ];
+    const PROBLEMS_DIFFICULTY_SHORT = [
+      '无',
+      '入门',
+      '普及-',
+      '提高-',
+      '提高',
+      '省选-',
+      '省选',
+      'NOI',
+      'NOI+',
+      'NOI+',
+      'NOI+',
+    ];
+    const rgb = (r, g, b) => `#${r.toString(16).padStart(2, '0')}${g.toString(16)}${b.toString(16)}`;
+    const PROBLEMS_DIFFICULTY_COLOR = {
+      0: rgb(191, 191, 191),
+      1: rgb(254, 76, 97),
+      2: rgb(243, 156, 17),
+      3: rgb(255, 193, 22),
+      4: rgb(82, 196, 26),
+      5: rgb(52, 152, 219),
+      6: rgb(157, 61, 207),
+      7: rgb(14, 29, 105),
+      8: rgb(14, 29, 105),
+      9: rgb(14, 29, 105),
+      10: rgb(14, 29, 105),
+    };
+    const clampLevel = (lv) => {
+      lv = Number(lv);
+      if (!Number.isFinite(lv)) lv = 0;
+      if (lv < 0) lv = 0;
+      if (lv > 10) lv = 10;
+      return Math.round(lv);
+    };
+    const difficultyLabel = (lv) => PROBLEMS_DIFFICULTY[clampLevel(lv)];
+    const difficultyLabelShort = (lv) => PROBLEMS_DIFFICULTY_SHORT[clampLevel(lv)];
+    const difficultyColor = (lv) => PROBLEMS_DIFFICULTY_COLOR[clampLevel(lv)];
+    this.addGlobal('difficultyLabel', difficultyLabel);
+    this.addGlobal('difficultyLabelShort', difficultyLabelShort);
+    this.addGlobal('difficultyColor', difficultyColor);
   }
 }
 nunjucks.runtime.memberLookup = function memberLookup(obj, val) {
